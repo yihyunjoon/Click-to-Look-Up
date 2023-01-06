98f2f373b9f7d46bffb9fefe1a8205eb7bd43418
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2023-01-07 04:58:24 +0900

    Menu bar

--- ClickToLookUp/ClickToLookUpApp.swift
+++ ClickToLookUp/ClickToLookUpApp.swift
@@ -6,8 +6,10 @@ struct ClickToLookUpApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("Click to Look Up App", systemImage: "character.book.closed.fill") {
+            Button("Quit Click to Look Up") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
         }
     }
 }
